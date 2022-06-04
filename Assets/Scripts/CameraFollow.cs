f27255a46af63f60987d553a8abb401d545a3110
Untouchez <65210494+Untouchez@users.noreply.github.com>
2022-06-04 15:45:40 -0700

    scene bug fix

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -5,10 +5,17 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     public Transform target;
-
+    public static Transform instance;
+    public float rotSpeed;
     // Update is called once per frame
+
+    void Start()
+    {
+        instance = this.transform;
+    }
     void Update()
     {
         transform.position = target.position;
+        transform.Rotate(new Vector3(0, Input.GetAxis("Mouse X") *Time.deltaTime*rotSpeed, 0));
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,32 +5,55 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     Animator anim;
-    public float acceleration = 2;
-    public float decceleration = 4;
-    public Vector3 rawInput;
-    public Vector3 input;
+    Transform cameraFollow;
+
+    [HideInInspector] public bool canMove;
+    float acceleration = 10;
+    float decceleration = 12;
+    Vector3 rawInput;
+    Vector3 input;
     // Start is called before the first frame update
     void Start()
     {
         anim = GetComponent<Animator>();
+        cameraFollow = CameraFollow.instance;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Inputs();
+        Locomotion();
+        Aiming();
+        if (Input.GetMouseButton(0))
+            anim.SetTrigger("attack");
+    }
+
+    public void Aiming()
+    {
+        transform.forward = cameraFollow.forward;
+    }
+
+    public void Locomotion()
+    {
+        rawInput = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0);
+        if (rawInput != Vector3.zero)
+            input = Vector3.Slerp(input, rawInput, acceleration * Time.deltaTime);
+        else
+            input = Vector3.Slerp(input, Vector3.zero, decceleration * Time.deltaTime);
+
+        if (canMove)
+            return;
         anim.SetFloat("InputX", input.x);
         anim.SetFloat("InputY", input.y);
     }
 
-    public void Inputs()
+    public void FootR()
     {
-        rawInput = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0);
-        if (rawInput != Vector3.zero) {
-            input = Vector3.Slerp(input, rawInput, acceleration*Time.deltaTime);
-        }else
-            input = Vector3.Slerp(input, Vector3.zero, decceleration*Time.deltaTime);
+
     }
 
+    public void FootL()
+    {
 
+    }
 }
